eea0896c7613365d61d77aa29f888321573b2828
Seth Platt <contact.sethjplatt@gmail.com>
2022-07-10 14:52:36 -0500

    all functionalities working and all required inputs accounted for.

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 let contacts = [
   {
-    name: "seth",
-    address: "2nd st",
-    id: "seth2nd st",
+    firstName: "Seth",
+    lastName: "Platt",
+    phoneNumber: "4258306261",
+    address: "600 S 1st St",
   },
   {
-    name: "steve",
-    address: "5th ave",
-    id: "steve5th ave",
+    firstName: "Paul",
+    lastName: "Jones",
+    phoneNumber: "2065415602",
+    address: "10 Lake Rd",
   },
 ];
 
 function addContact(name, address) {
   let newContact = {
-    name: document.getElementById("first-name").value,
+    firstName: document.getElementById("first-name").value,
+    lastName: document.getElementById("last-name").value,
+    phoneNumber: document.getElementById("phone-number").value,
     address: document.getElementById("address").value,
-    id: name + address,
   };
   contacts.push(newContact);
   clear();
-  displayContact(contacts);
+  displayContacts(contacts);
 }
 
 function clear() {
@@ -31,24 +34,36 @@ let searchInput = document.getElementById("search-bar");
 searchInput.addEventListener("input", function (e) {
   let currentSearch = e.target.value;
   let filteredContacts = contacts.filter((contact) => {
-    return contact.name.includes(currentSearch);
+    return (
+      contact.firstName.includes(currentSearch) ||
+      contact.lastName.includes(currentSearch) ||
+      contact.phoneNumber.includes(currentSearch) ||
+      contact.address.includes(currentSearch)
+    );
   });
   clear();
-  displayContact(filteredContacts);
+  displayContacts(filteredContacts);
 });
 
-function displayContact(contacts) {
+function displayContacts(contacts) {
   let contactsElms = document.createElement("ul");
   let contactListElm = contacts.map(function (contact) {
     let wrapper = document.createElement("li");
-    let name = document.createElement("span");
+    let firstName = document.createElement("span");
+    let lastName = document.createElement("span");
+    let phoneNumber = document.createElement("span");
     let address = document.createElement("span");
     let deleteButton = document.createElement("button");
 
-    name.innerHTML = contact.name;
+    firstName.innerHTML = contact.firstName;
+    lastName.innerHTML = contact.lastName;
+    phoneNumber.innerHTML = contact.phoneNumber;
     address.innerHTML = contact.address;
     deleteButton.innerHTML = "Delete";
-    wrapper.appendChild(name);
+
+    wrapper.appendChild(firstName);
+    wrapper.appendChild(lastName);
+    wrapper.appendChild(phoneNumber);
     wrapper.appendChild(address);
     wrapper.appendChild(deleteButton);
     contactsElms.appendChild(wrapper);
